28843af10b9776a336cc22ac4fb3a72e949fb104
Eugenia Liu <iameugenialiu@gmail.com>
2020-11-20 12:39:01 -0800

    fixed the joining of decimal IP addr

--- IPSnetCalc.py
+++ IPSnetCalc.py
@@ -1,5 +1,5 @@
 '''
-IPv4 Subnet Calculator
+Subnet Calculator (currently only IPv4...working on IPv6)
 takes IP and Subnet, returns relevant information
 by Eugenia Liu
 '''
@@ -8,9 +8,8 @@ by Eugenia Liu
 subnet_dict = {0:0, 1:128, 2:192, 3:224, 4:240, 5:248, 6:252, 7:254, 8:255}
 
 
-#gets the value of bits based on the decimal value (gets key vased on value in subnet_dict)
-def getKey(val):
-    for key,value in subnet_dict.items():
+def getKey(val, dicto):
+    for key,value in dicto.items():
         if val == value:
             return key
 
@@ -18,57 +17,55 @@ def getKey(val):
 #Given DDN/CIDR notation, converts to the other
 def snMaskConverter(subnet_mask):
     #Converts DDN to CIDR
-    if "." in subnet_mask:
-        subnet = subnet_mask.split(".")
+    if '.' in subnet_mask:
+        subnet = subnet_mask.split('.')
         prefix_sum = 0
         for octet in subnet:
-            prefix_sum += getKey(int(octet))
-        converted_subnet = ("/" + str(prefix_sum))
+            prefix_sum += getKey(int(octet), subnet_dict)
+        converted_subnet = '/' + str(prefix_sum)
     #Converts CIDR to DDN
-    if "/" in subnet_mask:
+    if '/' in subnet_mask:
         subnet = int(subnet_mask[1:])
         q,r = divmod(subnet,8)
-        converted_subnet = ("255."*q) + str(subnet_dict[r])
-        octet_count = converted_subnet.count(".")
+        converted_subnet = ('255.'*q) + str(subnet_dict[r])
+        octet_count = converted_subnet.count('.')
         if octet_count<3:
-            converted_subnet += (".0"*(3-octet_count))
+            converted_subnet += ('.0'*(3-octet_count))
     return converted_subnet
 
 
 #keeps or converts to CIDR notation when necessary
 def getPrefixMask(subnet_mask):
-    if "." in subnet_mask:
+    if '.' in subnet_mask:
         subnet_mask = snMaskConverter(subnet_mask)
     return int(subnet_mask[1:])
 
 
 #keeps or converts to DDN when necessary
 def getDecMask(subnet_mask):
-    if "/" in subnet_mask:
+    if '/' in subnet_mask:
         subnet_mask = snMaskConverter(subnet_mask)
     return subnet_mask
 
 
 #calculates wildcard mask given subnet mask decimal
 def getWildcardMask(subnet_mask):
-    snet_dec = getDecMask(subnet_mask).split(".")
-    wildcard_dec = ''
-    for octet in snet_dec:
-        wildcard_dec += (str(255-int(octet)) + ".")
-    return  wildcard_dec[:-1]
+    snet_dec = getDecMask(subnet_mask).split('.')
+    wildcard_dec = [str(255-int(octet)) for octet in snet_dec]
+    return  '.'.join(wildcard_dec)
 
 
-#calculates the total number of hosts, including network and broadcast
+#calculates the total number of usable addresses
 def getNumAddr(subnet_mask):
     subnet_mask = getPrefixMask(subnet_mask)
-    host_bits= 32-subnet_mask
-    num_hosts= pow(2,host_bits)
+    host_bits = 32-subnet_mask
+    num_hosts = pow(2,host_bits)
     return num_hosts
 
 
 #Given DDN IP Address, converts to binary address each octet decimal is an 8 digit binary value)
 def getBinAddr(ip_addr):
-    ip_addr = ip_addr.split(".")
+    ip_addr = ip_addr.split('.')
     binary_addr = ''
     for octet in ip_addr:
         binary_addr += format(int(octet), '08b')
@@ -93,14 +90,12 @@ def getNetBcastBin(ip_addr, subnet_mask):
 #Converts a binary value to decimal
 def binToDec(ip_addr):
     octets = []
-    dec_addr = ''
     n=8
     for octet in range (0,len(ip_addr),n):
         octets.append(ip_addr[octet:(octet+n)])
-    for binary in octets:
-        dec_addr += (str(int(binary,2)) + ".")
-    return dec_addr[:-1]
-
+    dec_addr = [str(int(octet,2)) for octet in octets]
+    return '.'.join(dec_addr)
+    
 
 #gets the network and brodacast address in DDN
 def getNetBcastDec(ip_addr, subnet_mask):
@@ -111,23 +106,17 @@ def getNetBcastDec(ip_addr, subnet_mask):
 
 #given network and bcast decimal, returns list of first and lat address in usable range
 def getUsableRange(ip_addr, subnet_mask):
-    net_dec = getNetBcastDec(ip_addr,subnet_mask)[0].split(".")
-    bcast_dec = getNetBcastDec(ip_addr,subnet_mask)[1].split(".")
-    net_dec[-1] = int(net_dec[-1])+1
-    bcast_dec[-1] = int(bcast_dec[-1])-1
-    first_str = ''
-    last_str = ''
-    for octets in net_dec:
-        first_str+=str(str(octets) + ".")
-    for octets in bcast_dec:
-        last_str+=str(str(octets) + ".")
-    return [first_str[:-1], last_str[:-1]]
+    net_dec = [str(octet) for octet in getNetBcastDec(ip_addr,subnet_mask)[0].split('.')]
+    bcast_dec = [str(octet) for octet in getNetBcastDec(ip_addr,subnet_mask)[1].split('.')]
+    net_dec[-1] = str(int(net_dec[-1])+1)
+    bcast_dec[-1] = str(int(bcast_dec[-1])-1)
+    return [('.'.join(net_dec)), ('.').join(bcast_dec)]
 
 
 #displays all relevant information given IP (DDN) and subnet mask (CIDR or prefix)
-def IPsubnetCalculator():
-    ip_addr = input("IP Address: ")
-    subnet_mask = input("Subnet mask: ")
+def SubnetCalculator():
+    ip_addr = input('IP Address: ')
+    subnet_mask = input('Subnet mask (x.x.x.x or /x): ')
     wildcard_bits = 32-getPrefixMask(subnet_mask)
     net = getNetBcastDec(ip_addr,subnet_mask)[0]
     bcast = getNetBcastDec(ip_addr,subnet_mask)[1]
@@ -135,14 +124,14 @@ def IPsubnetCalculator():
     hosts = int(getNumAddr(subnet_mask))
     print()
     print()
-    print("CIDR notation: " + ip_addr + "/" + str(getPrefixMask(subnet_mask)))
-    print("Subnet Mask: /" + str(getPrefixMask(subnet_mask)) + " or " + getDecMask(subnet_mask))
-    print("Wildcard Mask: /"  + str(wildcard_bits) + " or " + getWildcardMask(getDecMask(subnet_mask)))
-    print("Network address: " + net)
-    print("Usable IP Range: " + str(usable[0]) + " - " + str(usable[1]))
-    print("Broadcast address: " + bcast)
-    print("Number of Addresses: " + str(hosts))
-    print("Number of Usable Hosts: " + str(hosts-2))
+    print('CIDR notation: ' + ip_addr + '/' + str(getPrefixMask(subnet_mask)))
+    print('Subnet Mask: /' + str(getPrefixMask(subnet_mask)) + ' or ' + getDecMask(subnet_mask))
+    print('Wildcard Mask: /'  + str(wildcard_bits) + ' or ' + getWildcardMask(getDecMask(subnet_mask)))
+    print('Network address: ' + net)
+    print('Usable IP Range: ' + str(usable[0]) + ' - ' + str(usable[1]))
+    print('Broadcast address: ' + bcast)
+    print('Number of Addresses: ' + str(hosts))
+    print('Number of Usable Hosts: ' + str(hosts-2))
 
 
-IPsubnetCalculator()
+SubnetCalculator()
