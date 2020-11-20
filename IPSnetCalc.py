60d0bb4202b65fdc144a6446faf50960ca6a4e88
Eugenia Liu <iameugenialiu@gmail.com>
2020-11-20 09:42:17 -0800

    updated some methods
    combined net/bcast decimal method, edited some comments etc.

--- IPSnetCalc.py
+++ IPSnetCalc.py
@@ -7,23 +7,24 @@ by Eugenia Liu
 #dict of the 9 possible values of each octet of a subnet mask
 subnet_dict = {0:0, 1:128, 2:192, 3:224, 4:240, 5:248, 6:252, 7:254, 8:255}
 
+
 #gets the value of bits based on the decimal value (gets key vased on value in subnet_dict)
-def get_key(val):
+def getKey(val):
     for key,value in subnet_dict.items():
         if val == value:
             return key
 
 
-#Given CIDR/prefix notation, converts to the other
+#Given DDN/CIDR notation, converts to the other
 def snMaskConverter(subnet_mask):
-    #Converts CIDR to prefix notation
+    #Converts DDN to CIDR
     if "." in subnet_mask:
         subnet = subnet_mask.split(".")
         prefix_sum = 0
         for octet in subnet:
-            prefix_sum += get_key(int(octet))
+            prefix_sum += getKey(int(octet))
         converted_subnet = ("/" + str(prefix_sum))
-    #Converts prefix to CIDR notation
+    #Converts CIDR to DDN
     if "/" in subnet_mask:
         subnet = int(subnet_mask[1:])
         q,r = divmod(subnet,8)
@@ -38,13 +39,13 @@ def snMaskConverter(subnet_mask):
 def prefixMask(subnet_mask):
     if "." in subnet_mask:
         subnet_mask = snMaskConverter(subnet_mask)
-    return subnet_mask
+    return int(subnet_mask[1:])
 
 
 #calculates the total number of hosts, including network and broadcast
 def numHosts(subnet_mask):
     subnet_mask = prefixMask(subnet_mask)
-    host_bits= 32-int(subnet_mask[1:])
+    host_bits= 32-subnet_mask
     num_hosts= pow(2,host_bits)
     return num_hosts
 
@@ -61,7 +62,7 @@ def getBinAddr(ip_addr):
 #Given binary IP Address and subnet mask, gets the network and broadcast address
 def getNetBcastBin(ip_addr, subnet_mask):
     binary_addr = getBinAddr(ip_addr)
-    subnet_mask = int(prefixMask(subnet_mask)[1:])
+    subnet_mask = prefixMask(subnet_mask)
     net_addr = ''
     bcast_addr = ''
     for digits in binary_addr[:subnet_mask]:
@@ -85,22 +86,17 @@ def binToDec(ip_addr):
     return dec_addr[:-1]
 
 
-#gets the network address decimal
-def netAddrDec(ip_addr, subnet_mask):
-    bin_addr = getNetBcastBin(ip_addr,subnet_mask)[0]
-    return binToDec(bin_addr)
-
-
-#gets the broadcast address decimal
-def bcastAddrDec(ip_addr, subnet_mask):
-    bin_addr = getNetBcastBin(ip_addr,subnet_mask)[1]
-    return binToDec(bin_addr)
+#gets the network and brodacast address in DDN
+def getNetBcastDec(ip_addr, subnet_mask):
+    net_dec = binToDec(getNetBcastBin(ip_addr,subnet_mask)[0])
+    bcast_dec = binToDec(getNetBcastBin(ip_addr,subnet_mask)[1])
+    return [net_dec, bcast_dec]
 
 
 #given network and bcast decimal, returns list of first and lat address in usable range
-def usableRange(ip_addr, subnet_mask):
-    net_dec = netAddrDec(ip_addr,subnet_mask).split(".")
-    bcast_dec = bcastAddrDec(ip_addr,subnet_mask).split(".")
+def getUsableRange(ip_addr, subnet_mask):
+    net_dec = getNetBcastDec(ip_addr,subnet_mask)[0].split(".")
+    bcast_dec = getNetBcastDec(ip_addr,subnet_mask)[1].split(".")
     first = int(net_dec[-1])+1
     last = int(bcast_dec[-1])-1
     first_str = ''
@@ -117,10 +113,10 @@ def usableRange(ip_addr, subnet_mask):
 #displays all relevant information given IP (DDN) and subnet mask (CIDR or prefix)
 def IPsubnetCalculator():
     ip_addr = input("IP Address: ")
-    subnet_mask= input("Subnet mask: ")
-    net = netAddrDec(ip_addr, subnet_mask)
-    bcast = bcastAddrDec(ip_addr, subnet_mask)
-    usable = usableRange(ip_addr,subnet_mask)
+    subnet_mask = input("Subnet mask: ")
+    net = getNetBcastDec(ip_addr,subnet_mask)[0]
+    bcast = getNetBcastDec(ip_addr,subnet_mask)[1]
+    usable = getUsableRange(ip_addr,subnet_mask)
     hosts = int(numHosts(subnet_mask))
     print()
     print()
